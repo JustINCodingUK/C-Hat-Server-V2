a21679344a3da8c3cde5ce7c159e0dd164aa20f1
JustinW <youdontneedtoknowmyemail@google.com>
2024-01-18 17:58:08 +0000

    Add config reader for plugin jars

--- core/src/main/kotlin/io/github/justincodinguk/c_hat_server_v2/core/plugin_manager/PluginManager.kt
+++ core/src/main/kotlin/io/github/justincodinguk/c_hat_server_v2/core/plugin_manager/PluginManager.kt
@@ -1,30 +1,54 @@
 package io.github.justincodinguk.c_hat_server_v2.core.plugin_manager
 
+import io.github.justincodinguk.c_hat_server_v2.eventbus.Event
 import io.github.justincodinguk.c_hat_server_v2.eventbus.EventBus
+import io.github.justincodinguk.c_hat_server_v2.eventbus.OnEventCallback
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.URLClassLoader
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.jar.JarFile
 import kotlin.io.path.name
+import kotlin.io.path.pathString
+
 
 class PluginManager(private val eventBus: EventBus) {
 
     fun loadPluginsFromDirectory(directory: String) {
         for(file in Files.walk(Paths.get(directory))) {
             if(file.name.endsWith(".jar")) {
-                val pluginId = file.name.split("-v")[0]
-                val classLoader = URLClassLoader(arrayOf(file.toUri().toURL()))
-                val runnerClass = classLoader.loadClass("io.github.justincodinguk.c_hat_server_v2.external_plugins.PluginRunner")
-                val runnerClassInstance = runnerClass.getConstructor().newInstance()
-                runnerClass.methods.single {
-                    it.name == "${pluginId}Runner"
-                }.invoke(runnerClassInstance)
+                try {
+                    val jarFile = JarFile(file.pathString)
+                    val pluginConfStream = jarFile.getInputStream(jarFile.getEntry("plugins.json"))
+                    val pluginConf = Json.decodeFromString<PluginConfig>(BufferedReader(InputStreamReader(pluginConfStream)).readText())
+
+                    val classLoader = URLClassLoader(arrayOf(file.toUri().toURL()))
+                    val runnerClass = classLoader.loadClass(pluginConf.pluginClassFullyQualifiedName)
+
+                    val runnerClassInstance = runnerClass.getConstructor().newInstance()
+                    val allowedMethods = buildMap {
+                        Event.values().forEach { put(it.toFunctionName(), it) }
+                    }
+
+                    runnerClass.methods.forEach {
+                        if(it.name in allowedMethods.keys) {
+                            val event = allowedMethods[it.name]!!
+                            eventBus.registerTask(event, OnEventCallback(pluginConf.pluginId, it, runnerClassInstance))
+                        }
+                    }
+
+                } catch(e: Exception) {
+                    //throw IllegalArgumentException("Invalid plugin ${file.name}")
+                    throw e
+                }
             }
         }
     }
 
-    fun dynamicUnload(pluginId: String) {
-
-    }
+    fun dynamicUnload(pluginId: String) = eventBus.unregisterCallbacksFor(pluginId)
 
     fun loadFromSource(pluginId: String) {
         try {
--- eventbus/src/main/kotlin/io/github/justincodinguk/c_hat_server_v2/eventbus/OnEventCallback.kt
+++ eventbus/src/main/kotlin/io/github/justincodinguk/c_hat_server_v2/eventbus/OnEventCallback.kt
@@ -1,7 +1,18 @@
 package io.github.justincodinguk.c_hat_server_v2.eventbus
 
-sealed interface OnEventCallback<in T> {
-    val pluginId: String
-    val callback: T.() -> Unit
+import java.lang.reflect.Method
+
+data class OnEventCallback(
+    val pluginId: String,
+    val callback: Method,
+    val classInstance: Any
+) {
+    fun invokeCallback(eventObj: Any?) {
+        if(eventObj==null) {
+            callback.invoke(classInstance)
+        } else {
+            callback.invoke(classInstance, eventObj)
+        }
+    }
 }
 
